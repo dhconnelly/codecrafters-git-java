3afeff8437ada732c6ab2b03b240a98986755c0e
Daniel Connelly <dhconnelly@gmail.com>
2024-04-20 09:09:49 +0200

    add testing

---
+++ src/test/java/GitTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GitTest {
+  @Test
+  public void testFoo() {
+    final String foo = "foo";
+    assertEquals("foo", foo);
+  }
+}
